ad416e5ab61b40e4f765007d4dc942e77d84b76f
Tinker <christiankroh@web.de>
2013-12-06 20:42:32 +0100

    rest and others

---
+++ db_access.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import fdb
+import fdb.tuple
+import sys
+import struct
+sys.path.append('./python-layers/lib')
+import
---
+++ rest-api/run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from eve import Eve
+app = Eve()
+
+if __name__ == '__main__':
+    app.run()
