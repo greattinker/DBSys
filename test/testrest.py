3fb058df65c33d1ba1f454b8757bc32a6179de55
tinker <christiankroh@web.de>
2014-01-15 14:22:56 +0100

    d

--- test/testrest.py
+++ test/testrest.py
@@ -3,12 +3,12 @@
 import httplib, urllib
 import json
 
-params = json.dumps( {"username": 'user_1', 'created': '1389791776464'} )
+params = json.dumps( {"username": 'user_1'} )
 print params
 
 headers = {"Content-type": "application/json", "Accept": "*/*"}
 conn = httplib.HTTPConnection("localhost:5000")
-conn.request("POST", "/post_tweet", params, headers)
+conn.request("POST", "/tweets", params, headers)
 
 response = conn.getresponse()
 print response.status, response.read()
