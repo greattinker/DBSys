9191e0a11a28bcb617076056583f8cc11410e7c9
tinker <christiankroh@web.de>
2014-01-15 02:36:37 +0100

    d

--- twitter2.py
+++ twitter2.py
@@ -125,10 +125,10 @@ class tweet(twitter):
 #		while len(tweets) < 40 and len(alltweets) > 0:
 #			tweets.append(alltweets.pop())
 		tweets = []		
-		for k,v in tr.get_range_startswith(self._tweets_space.pack((username,)), 40, True):
+		for k,v in tr.get_range_startswith(self._tweets_space.pack((username,)).key(), 40, True):
 			friend = v
 			body = tr[self._tweet_space.pack((str(friend),fdb.tuple.unpack(k)[3]))]
-			tweets.append(str(k))
+			tweets.append([str(k)])
 			tweets.append([datetime.fromtimestamp(fdb.tuple.unpack(k)[3]/1000),str(friend),str(body)])
 		return tweets
 
