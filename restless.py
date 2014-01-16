7c88b022236ef58cf5ca6b47d7297428ef93a1f6
tinker <christiankroh@web.de>
2014-01-16 15:03:30 +0100

    d

--- restless.py
+++ restless.py
@@ -12,12 +12,12 @@ app = Flask(__name__)
 @app.route("/clear", methods = ["POST"])
 def clear():
 	tweet.cleanAll()
-	return "", 201
+	return "", 200
 	
 @app.route("/tweets", methods = ["POST"])
 def get_tweetspost():
 	username = request.json["username"]
-	return jsonify( { username: tweet.getTweetsForUser(str(username), 0, 40) })
+	return jsonify( { username: tweet.getTweetsForUser(str(username), 0, 40) }), 200
 
 @app.route("/tweets/<string:username>", methods = ["GET"])
 def get_tweets(username):
@@ -28,41 +28,41 @@ def post_tweet():
 	if not request.json or not "username" in request.json:
 		abort(400)
 	tweet.addTweet(str(request.json["username"]), None, str(request.json["body"]))
-	return "", 201
-	
+	return "", 200
+		
 @app.route("/create_user", methods = ["POST"])
 def create_user():
 	user.addUser(request.json["username"], request.json["password"])
-	return "", 201
+	return "", 200
 	
 @app.route("/add_friend", methods = ["POST"])
 def add_friend():
 	follow.follows(request.json["username"],request.json["friend"])
-	return "", 201
+	return "", 200
 	
 @app.route("/import_friends", methods = ["POST"])
 def import_friends():
 	friends = request.json["friends"]
 	username = request.json["username"]
 	follow.import_follows(friends,username)
-	return "", 201
+	return "", 200
 	
 @app.route("/import_tweets", methods = ["POST"])
 def import_tweets():
 	bodies = request.json["bodies"]
 	timestamps = request.json["timestamps"]
 	tweet.import_tweets(str(request.json["username"]), timestamps, bodies)
-	return "", 201
+	return "", 200
 
 @app.route("/get_friends", methods = ["POST"])
 def get_friends():
 	username = request.json["username"]
-	return jsonify( { username: follow.getFollowing(username)})
+	return jsonify( { username: follow.getFollowing(username)}), 200
 	
 @app.route("/get_followers", methods = ["POST"])
 def get_followers():
 	username = request.json["username"]
-	return jsonify( { username: follow.getFollowersOfUser(username)}) 
+	return jsonify( { username: follow.getFollowersOfUser(username)}),200
 	
 if __name__ == "__main__":
 	app.run(debug = True)
