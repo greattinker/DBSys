9425f0e578e8be4869b551a68e086da74dd6b832
Tinker <christiankroh@web.de>
2014-01-21 15:34:05 +0100

    sd

--- restless.py
+++ restless.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from flask import Flask, jsonify, request, abort
-from twitter2 import user, tweet, follow
+from twitter import user, tweet, follow
 
 tweet = tweet()
 user = user()
